3e2449dd7efbc55dec5bc1cd2e91a28e33b48b65
Subhash Singh <ss5138275@gmail.com>
2025-10-07 10:20:33 +0545

    all orders

--- lib/screens/check_single_order_screen.dart
+++ lib/screens/check_single_order_screen.dart
@@ -1,6 +1,5 @@
 // // ignore_for_file: must_be_immutable, file_names, prefer_const_literals_to_create_immutables, prefer_const_constructors, prefer_interpolation_to_compose_strings
 
-
 // import 'package:flutter/material.dart';
 // import 'package:shopify_admin/utils/constant.dart';
 
@@ -75,8 +74,6 @@
 //     );
 //   }
 // }
-
-
 
 // ignore_for_file: must_be_immutable, file_names, prefer_const_literals_to_create_immutables, prefer_const_constructors, prefer_interpolation_to_compose_strings
 
